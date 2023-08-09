495c9d198a5cfcef64448be0e6ccb3880134133f
Gupta79 <98534438+Gupta79@users.noreply.github.com>
2023-08-09 19:41:54 +0530

    Add files via upload

---
+++ Lecture4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+long long int a;
+int b;
+
+cout<<"The size of int is :"<<sizeof(b)<< "The size of long long int is:"<<sizeof(a);
+return 0;
+}
