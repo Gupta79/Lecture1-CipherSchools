ced82be9766a498a837ba4c36bcd9b30ba558221
Gupta79 <98534438+Gupta79@users.noreply.github.com>
2023-08-09 19:27:28 +0530

    Create Lecture3_part2.cpp

---
+++ Lecture3_part2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=3;
+    int b=5;
+
+    // a=3;
+    // b=5;
+
+    // int c;
+    // c=a+b;
+    
+    cout<<a+b<<endl<<"a+b";
+    return 0;
+}
