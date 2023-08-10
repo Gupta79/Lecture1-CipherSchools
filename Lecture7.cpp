a6b17343665549f7a9f41d5605d95adc2ad5d02c
Gupta79 <98534438+Gupta79@users.noreply.github.com>
2023-08-10 20:11:05 +0530

    Lecture7.cpp

--- Lecture7.cpp
+++ Lecture7.cpp
@@ -3,14 +3,7 @@ using namespace std;
 int main(){
     int password;
     cout<<"enter the password";
-    // cin>>password;
-    // //using while loop
-    // while(password<999999){
-    //     cout<<"the password does not meet condition";
-    //     cin>>password; 
-    // }
-
-    //using do while loop
+    
     int password2;
     do{
         cin>>password2;
