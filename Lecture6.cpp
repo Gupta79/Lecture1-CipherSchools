7e7c207414db1452a7850c7958d5ddb5f1081410
Gupta79 <98534438+Gupta79@users.noreply.github.com>
2023-08-10 20:17:31 +0530

    Lecture6.cpp

--- Lecture6.cpp
+++ Lecture6.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //printning natural numbers
+
     cout<<1<<endl;
     cout<<2<<endl;
     cout<<3<<endl;
-    //to remove this repetition we will use loops
-    //For Loop
+
     int n;
     cin>>n;
     for(int i=1; i<=n; i++){
