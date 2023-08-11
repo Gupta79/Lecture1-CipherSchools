663affd66ec0b8dea4334301874c14df8ecd0d10
Gupta79 <98534438+Gupta79@users.noreply.github.com>
2023-08-11 14:59:12 +0530

    Add files via upload

---
+++ Lecture8_part2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int mul(int a , int b){
+    return (a*b);
+}
+
+void print(){
+    cout<<"this funtion returns nothing";
+}
+int main(){
+    print();
+    return 0;
+}
